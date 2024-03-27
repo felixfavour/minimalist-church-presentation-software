19e043ec1ee90ef20436679d99280a370c10194b
felixfavour <felixfavour0@gmail.com>
2024-03-27 13:49:05 +0100

    chore: updated font size logic to use interpolation

--- composables/useScreenFontSize.ts
+++ composables/useScreenFontSize.ts
@@ -1,24 +1,30 @@
 const useScreenFontSize = (content: string) => {
-  let fontSize = 5
-  if (content.length <= 150) {
-    fontSize = 5
-  } else if (content.length < 200) {
-    fontSize = 4.7
-  } else if (content.length < 250) {
-    fontSize = 4.4
-  } else if (content.length < 300) {
-    fontSize = 4.1
-  } else if (content.length < 350) {
-    fontSize = 3.8
-  } else if (content.length < 400) {
-    fontSize = 3.5
-  } else if (content.length < 450) {
-    fontSize = 3.1
-  } else {
-    fontSize = 2.8
-  }
+  // if (content.length <= 150) {
+  //   fontSize = 5
+  // } else if (content.length < 200) {
+  //   fontSize = 4.7
+  // } else if (content.length < 250) {
+  //   fontSize = 4.4
+  // } else if (content.length < 300) {
+  //   fontSize = 4.1
+  // } else if (content.length < 350) {
+  //   fontSize = 3.8
+  // } else if (content.length < 400) {
+  //   fontSize = 3.5
+  // } else if (content.length < 450) {
+  //   fontSize = 3.1
+  // } else {
+  //   fontSize = 2.8
+  // }
 
-  return fontSize
+  /**
+   * Using interpolation to estimate font size
+   * where 525 is the highest content length and 5vw is the corresponding font size
+   * and 125 is the lowest content length and 2.8vw is the corresponding font size
+   */
+  const fontSize = 5 + (content.length - 125) * (2.8 - 5) / (525 - 125)
+
+  return fontSize.toFixed(2)
 }
 
 export default useScreenFontSize;
