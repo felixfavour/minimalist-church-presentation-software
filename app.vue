a0292a081d92771ecc6e6c6f8b45ccda5d565e50
felixfavour <felixfavour0@gmail.com>
2024-05-03 16:28:18 +0100

    chore: reroute app-init logic to layouts/app.vue

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <NuxtLoadingIndicator />
 
-    <NuxtLayout v-if="!loadingResources" :app-version="appVersion">
+    <NuxtLayout :app-version="appVersion">
       <NuxtPage />
       <UNotifications>
         <template #title="{ title }">
@@ -24,33 +24,6 @@
         </template>
       </UNotifications>
     </NuxtLayout>
-    <div
-      v-else
-      class="loading-ctn h-[100vh] w-[100vw] fixed inset-0 grid place-items-center"
-    >
-      <div class="wrapper flex flex-col gap-2">
-        <div class="logo flex items-center justify-center mb-6 gap-2">
-          <Logo class="w-[48px]" />
-          <h1 class="text-2xl font-semibold">Cloud of Worshippers</h1>
-        </div>
-        <div class="progress-wrapper text-center">
-          <UProgress
-            size="2xl"
-            class="text-center"
-            :value="downloadProgress"
-            :max="[
-              'Setting up environment...',
-              'Loading KJV Bible...',
-              'Loading NKJV Bible...',
-              'Loading NIV Bible...',
-              'Loading AMP Bible...',
-              'Loading Hymns...',
-              'Finishing up',
-            ]"
-          />
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -59,12 +32,6 @@ import mitt from "mitt"
 import type { AppSettings } from "./types"
 import { useAppStore } from "./store/app"
 
-import kjvBible from "./public/kjv.json"
-// import nkjvBible from "./public/nkjv.json"
-// import nivBible from "./public/niv.json"
-// import ampBible from "./public/amp.json"
-import hymns from "./public/hymns.json"
-
 const nuxtApp = useNuxtApp()
 const emitter = mitt()
 const appStore = useAppStore()
@@ -72,8 +39,6 @@ nuxtApp.provide("emitter", emitter)
 appStore.setEmitter(emitter)
 
 const appVersion = ref<string>("0.5.9")
-const loadingResources = ref<boolean>(true)
-const downloadProgress = ref<number>(5)
 
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
@@ -92,58 +57,6 @@ const registerServiceWorker = async () => {
       console.error(`Registration failed with ${error}`)
     }
   }
-}
-
-const downloadEssentialResources = async () => {
-  loadingResources.value = true
-  downloadProgress.value = 1
-
-  // Download KJV Bible
-  // const kjvBible = await useS3File("kjv.json")
-  useNuxtApp().provide("kjvBible", kjvBible || "")
-  downloadProgress.value = 2
-
-  // // Download NKJV Bible
-  // // const nkjvBible = await useS3File("nkjv.json")
-  // useNuxtApp().provide("nkjvBible", nkjvBible || "")
-  // downloadProgress.value = 3
-
-  // // Download NIV Bible
-  // // const nivBible = await useS3File("niv.json")
-  // useNuxtApp().provide("nivBible", nivBible || "")
-  // downloadProgress.value = 4
-
-  // // Download AMP Bible
-  // // const ampBible = await useS3File("amp.json")
-  // useNuxtApp().provide("ampBible", ampBible || "")
-  // downloadProgress.value = 5
-
-  // Download all hymns
-  // const hymns = await useS3File("hymns.json")
-  useNuxtApp().provide("hymns", hymns || "")
-  downloadProgress.value = 6
-
-  setTimeout(() => {
-    downloadProgress.value = 7
-    loadingResources.value = false
-  }, 500)
-}
-
-const retrieveAllMediaFilesFromDB = async () => {
-  const slides = [...appStore.activeSlides]
-  slides.forEach(async (slide) => {
-    if (slide.type === slideTypes.media) {
-      const mediaObj = await useIndexedDB()
-        .media.where({ id: slide.id })
-        .toArray()
-      if (mediaObj[0]) {
-        const fileUrl = URL.createObjectURL(mediaObj[0].content)
-        slide.data.url = fileUrl
-        slide.background = fileUrl
-        appStore.setActiveSlides(slides)
-      }
-    }
-  })
 }
 
 const overrideAppSettings = () => {
@@ -173,11 +86,6 @@ const overrideAppSettings = () => {
 }
 
 registerServiceWorker()
-
-downloadEssentialResources()
-
-retrieveAllMediaFilesFromDB()
-
 overrideAppSettings()
 </script>
 
--- layouts/app.vue
+++ layouts/app.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="app-ctn max-h-[100vh] overflow-hidden">
+  <div v-if="!loadingResources" class="app-ctn max-h-[100vh] overflow-hidden">
     <Navbar :app-version="appVersion" />
     <slot />
   </div>
+  <div
+    v-else
+    class="loading-ctn h-[100vh] w-[100vw] fixed inset-0 grid place-items-center"
+  >
+    <div class="wrapper flex flex-col gap-2">
+      <div class="logo flex items-center justify-center mb-6 gap-2">
+        <Logo class="w-[48px]" />
+        <h1 class="text-2xl font-semibold">Cloud of Worshippers</h1>
+      </div>
+      <div class="progress-wrapper text-center">
+        <UProgress
+          size="2xl"
+          class="text-center"
+          :value="downloadProgress"
+          :max="[
+            'Setting up environment...',
+            'Loading KJV Bible...',
+            'Loading NKJV Bible...',
+            'Loading NIV Bible...',
+            'Loading AMP Bible...',
+            'Loading Hymns...',
+            'Finishing up',
+          ]"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import kjvBible from "../public/kjv.json"
+// import nkjvBible from "../public/nkjv.json"
+// import nivBible from "../public/niv.json"
+// import ampBible from "../public/amp.json"
+import hymns from "../public/hymns.json"
+import { useAppStore } from "~/store/app"
+
 useHead({
   title: "Cloud of Worshippers",
 })
@@ -13,11 +47,64 @@ defineProps({
   appVersion: String,
 })
 
-// const router = useRouter()
-// const token = useCookie("token")
-// if (!token.value) {
-//   router.push("/login")
-// }
+const appStore = useAppStore()
+const loadingResources = ref<boolean>(true)
+const downloadProgress = ref<number>(5)
+
+const downloadEssentialResources = async () => {
+  loadingResources.value = true
+  downloadProgress.value = 1
+
+  // Download KJV Bible
+  // const kjvBible = await useS3File("kjv.json")
+  useNuxtApp().provide("kjvBible", kjvBible || "")
+  downloadProgress.value = 2
+
+  // // Download NKJV Bible
+  // // const nkjvBible = await useS3File("nkjv.json")
+  // useNuxtApp().provide("nkjvBible", nkjvBible || "")
+  // downloadProgress.value = 3
+
+  // // Download NIV Bible
+  // // const nivBible = await useS3File("niv.json")
+  // useNuxtApp().provide("nivBible", nivBible || "")
+  // downloadProgress.value = 4
+
+  // // Download AMP Bible
+  // // const ampBible = await useS3File("amp.json")
+  // useNuxtApp().provide("ampBible", ampBible || "")
+  // downloadProgress.value = 5
+
+  // Download all hymns
+  // const hymns = await useS3File("hymns.json")
+  useNuxtApp().provide("hymns", hymns || "")
+  downloadProgress.value = 6
+
+  setTimeout(() => {
+    downloadProgress.value = 7
+    loadingResources.value = false
+  }, 500)
+}
+
+const retrieveAllMediaFilesFromDB = async () => {
+  const slides = [...appStore.activeSlides]
+  slides.forEach(async (slide) => {
+    if (slide.type === slideTypes.media) {
+      const mediaObj = await useIndexedDB()
+        .media.where({ id: slide.id })
+        .toArray()
+      if (mediaObj[0]) {
+        const fileUrl = URL.createObjectURL(mediaObj[0].content)
+        slide.data.url = fileUrl
+        slide.background = fileUrl
+        appStore.setActiveSlides(slides)
+      }
+    }
+  })
+}
+
+downloadEssentialResources()
+retrieveAllMediaFilesFromDB()
 </script>
 
 <style scoped></style>
