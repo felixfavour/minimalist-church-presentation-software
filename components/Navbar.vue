54f38dc0079d00773cde257b1dfab99d86e487a8
felixfavour <felixfavour0@gmail.com>
2024-05-29 08:50:46 +0100

    chore: add changelog button trigger

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,11 +7,12 @@
       <div class="logo flex items-center gap-2">
         <Logo class="w-[32px]" />
         <h1 class="text-md font-semibold">Cloud of Worshippers</h1>
-        <span
-          class="version-chip flex text-xs font-semibold bg-gray-100 dark:bg-primary-900 p-2 py-1 rounded-full dark:bg-primary-900"
+        <UButton
+          class="version-chip flex text-xs font-semibold bg-primary-500 p-2 py-1 rounded-full border border-primary-500 hover:border-primary-900 transition-all"
+          @click="useGlobalEmit('show-changelog')"
         >
           v{{ appVersion }}
-        </span>
+        </UButton>
       </div>
       <div class="actions text-sm flex gap-2 items-center">
         <ClientOnly>
