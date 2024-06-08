d7997aa2697a1ca1d40e907a514994ffb3410703
felixfavour <felixfavour0@gmail.com>
2024-06-08 14:04:38 +0100

    chore: v0.7.0

--- components/ChangelogModal.vue
+++ components/ChangelogModal.vue
@@ -51,15 +51,12 @@ defineProps<{
 
 const emitter = useNuxtApp().$emitter as Emitter<any>
 const changelog = `
-- You can now add your own images and videos as backgrounds for your slides.
-- We've added support for your audio-only files for media slides. Just go ahead and create a media slide and see the customizations you can add to your media(audio) slide.
-- You can now play, pause and restart your media files with the Media slide controls.
-- One alert is not enough, we added an alert scheduler so you can store alerts and take any one live as you wish.
-- This one is exciting! We added a countdown timer for your convenience. Now you can let your congregation know what how soon an event starts or finish in an exciting manner.
-- Sharing is caring; now other people can use the songs you upload—but only if you want.
-- We don't want you to ever make a mistake taking slides LIVE. So we added a two-step feature to the "Display Bible" action on the quick actions pane.
-- Want to edit a live slide quickly, double tap the slide in the Live Output pane and make your quick adjustments ;)
-- Fixed some issues to make your CoW experience smoother!`
+- Full offline and PWA support
+- New and improved Bible search with filters
+- Fixed Disappearing slide background videos
+- New songs added
+- Added recently opened Bible verses in “Display Bible” action
+- Fixed minor bugs and issues for improved experience`
 
 const appStore = useAppStore()
 
