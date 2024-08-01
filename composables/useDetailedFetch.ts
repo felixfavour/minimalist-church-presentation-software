3d00447658da2bc9126dfb30ce61c48d8fdc9892
felixfavour <felixfavour0@gmail.com>
2024-08-01 15:51:30 +0100

    feat: download percents for initial setup

---
+++ composables/useDetailedFetch.ts
@@ -0,0 +1,40 @@
+async function useDetailedFetch(url: string, progressRef: Ref<string>) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const contentLength = response.headers.get('content-length');
+  const totalBytes = contentLength ? parseInt(contentLength, 10) : 0;
+  let loadedBytes = 0;
+
+  const reader = response?.body?.getReader();
+  const stream = new ReadableStream({
+    start(controller) {
+      function push() {
+        reader?.read().then(({ done, value }) => {
+          if (done) {
+            controller.close();
+            return;
+          }
+          loadedBytes += value.length;
+          progressRef.value = ((loadedBytes / totalBytes) * 100).toFixed(2)
+          // console.log(`Downloaded ${loadedBytes} of ${totalBytes} bytes (${((loadedBytes / totalBytes) * 100).toFixed(2)}%)`);
+          controller.enqueue(value);
+          push();
+        }).catch(error => {
+          console.error('Error in reading stream:', error);
+          controller.error(error);
+        });
+      }
+
+      push();
+    }
+  });
+
+  return new Response(stream, {
+    headers: response.headers
+  });
+}
+
+export default useDetailedFetch;
--- composables/useS3File.ts
+++ composables/useS3File.ts
@@ -1,7 +1,7 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 // Upload file to AWS S3
-const useS3File = async (fileName: string) => {
+const useS3File = async (fileName: string, progressRef: Ref<string>) => {
   const runtimeConfig = useRuntimeConfig()
 
   const client = new S3Client({
@@ -17,9 +17,41 @@ const useS3File = async (fileName: string) => {
     Key: fileName
   })
 
+  progressRef.value = "0"
+  let loadedBytes = 0
   const response = await client.send(command)
-  const str = await response.Body?.transformToString()
-  return JSON.parse(str)
+  // const str = await response.Body?.transformToString()
+  const contentLength = response.ContentLength
+  const totalBytes = contentLength || 0
+  const reader = response.Body?.transformToWebStream().getReader()
+
+  const stream = new ReadableStream({
+    start(controller) {
+      console.log('starting')
+      function push() {
+        reader?.read().then(({ done, value }) => {
+          if (done) {
+            controller.close();
+            return;
+          }
+          loadedBytes += value.length;
+          progressRef.value = ((loadedBytes / totalBytes) * 100).toFixed(2)
+          // console.log(`Downloaded ${loadedBytes} of ${totalBytes} bytes (${((loadedBytes / totalBytes) * 100).toFixed(2)}%)`);
+          controller.enqueue(value);
+          push();
+        }).catch(error => {
+          console.error('Error in reading stream:', error);
+          controller.error(error);
+        });
+      }
+
+      push();
+    }
+  });
+
+  const json = await new Response(stream).json()
+
+  return json;
 }
 
 export default useS3File
