73b6823b8fce4924565018a3792c6e27763f0fe8
felixfavour <felixfavour0@gmail.com>
2024-11-08 20:03:00 +0100

    chore: posthog plugin

--- plugins/posthog.ts
+++ plugins/posthog.ts
@@ -3,28 +3,13 @@ import { useAuthStore } from '~/store/auth'
 
 export default defineNuxtPlugin(nuxtApp => {
   const auth = useAuthStore()
-  console.log(auth.user)
-  posthog.init('phc_sZj5IKRKRCd6Mv8GrthWQOlWu4ihmCFry3oQAIKrW9T',
-    {
-      api_host: 'https://us.i.posthog.com',
-      person_profiles: 'identified_only'
-    }
-  )
-  posthog.identify(
-    auth.user?._id,
-    {
-      email: auth.user?.email,
-      name: auth.user?.fullname,
-    }
-  )
-
-  // if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
-  //   posthog.init('phc_sZj5IKRKRCd6Mv8GrthWQOlWu4ihmCFry3oQAIKrW9T',
-  //     {
-  //       api_host: 'https://us.i.posthog.com',
-  //       person_profiles: 'identified_only'
-  //     }
-  //   )
+  // console.log(auth.user)
+  // posthog.init('phc_sZj5IKRKRCd6Mv8GrthWQOlWu4ihmCFry3oQAIKrW9T',
+  //   {
+  //     api_host: 'https://us.i.posthog.com',
+  //     person_profiles: 'identified_only'
+  //   }
+  // )
   // posthog.identify(
   //   auth.user?._id,
   //   {
@@ -32,5 +17,20 @@ export default defineNuxtPlugin(nuxtApp => {
   //     name: auth.user?.fullname,
   //   }
   // )
-  // }
+
+  if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
+    posthog.init('phc_sZj5IKRKRCd6Mv8GrthWQOlWu4ihmCFry3oQAIKrW9T',
+      {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only'
+      }
+    )
+    posthog.identify(
+      auth.user?._id,
+      {
+        email: auth.user?.email,
+        name: auth.user?.fullname,
+      }
+    )
+  }
 })
